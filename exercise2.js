dcd7d4cf5cbc1ef1b42239f46d3cb7a2721d0f05
Melina Alejos <malejos1219@gmail.com>
2020-04-16 08:23:24 -0400

    second commit with updated solution

--- exercise2.js
+++ exercise2.js
@@ -1,13 +1,33 @@
 //Write a function, testNum, that takes a number as an argument and returns a Promise that tests if the value is less than or greater than 50.
 
-function textNum(number){
-	return new Promise(function(resolve,reject){
-		if((number < 50) || (number > 50)){
-			resolve(number);
+//********* ORIGINAL ATTEMPT---> did not work :( ******************
+
+// function textNum(number){
+// 	return new Promise(function(resolve,reject){
+// 		if((number < 50) || (number > 50)){
+// 			resolve(number);
+// 		} else {
+// 			reject(number);
+// 		}
+// 	})
+// }
+
+function compareToTen(num){
+	p = new Promise(function(resolve,reject){
+		if(num > 50){
+			resolve(num + " is greater than 50, success!")
 		} else {
-			reject(number);
+			reject(num + " is less than 50, fail!")
 		}
 	})
+	return p
 }
 
+//how to call the function
+compareToTen(55)
+	.then(result => console.log(result))
+	.catch(error => console.log(error))
 
+compareToTen(8)
+	.then(result => console.log(result))
+	.catch(error => console.log(error))
